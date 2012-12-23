e6942f45bd6d42dc27f94a64e11810c51206404a
joribly <jrblystone@gmail.com>
2012-12-23 17:22:27 -0500

    added roadSegment mate support to intersection class

--- Traffic/DataModel/src/layout/RoadLayout.java
+++ Traffic/DataModel/src/layout/RoadLayout.java
@@ -1,65 +1,29 @@
 package layout;
 
-import static layout.RoadElement.*;
-
 public enum RoadLayout {
 
     FOUR_LANE_STD(
-            HEADING_FROM,
-            DIVIDER_SOLID,
-            LANE_STD_PLUS_RIGHT_TURN,
-            DIVIDER_DOTTED,
-            LANE_STD_PLUS_LEFT_TURN,
-            HEADING_TO,
-            DIVIDER_SOLID,
-            LANE_STD_PLUS_LEFT_TURN,
-            DIVIDER_DOTTED,
-            LANE_STD_PLUS_RIGHT_TURN,
-            DIVIDER_SOLID),
+            Lane.createStandardFrom().setCanGoStraight().setCanGoRight().setCanChangeLaneLeft(),
+            Lane.createStandardFrom().setCanGoStraight().setCanGoLeft().setCanChangeLaneRight(),
+            Lane.createStandardTo().setCanGoStraight().setCanGoLeft().setCanChangeLaneRight(),
+            Lane.createStandardTo().setCanGoStraight().setCanGoRight().setCanChangeLaneLeft()
+    );
 
-    FOUR_LANE_STD_WITH_LEFT_TURN_LANES(
-            HEADING_FROM,
-            DIVIDER_SOLID,
-            LANE_STD,
-            DIVIDER_DOTTED,
-            LANE_STD,
-            HEADING_TO,
-            DIVIDER_SOLID,
-            LANE_STD_LEFT_TURN_ONLY,
-            DIVIDER_DOTTED,
-            LANE_STD,
-            DIVIDER_DOTTED,
-            LANE_STD,
-            DIVIDER_SOLID);
-
-
-    private RoadElement[] layout;
+    private Lane[] lanes;
     private int count;
 
-    RoadLayout(RoadElement... aLayout) {
-        layout = aLayout;
-        count = 0;
-        for(RoadElement item: layout) {
-            if(item.isLane())count++;
-        }
+    RoadLayout(Lane ... lanes) {
+        this.lanes = lanes;
+        count = lanes.length;
     }
 
     public String toString() {
         StringBuffer sb = new StringBuffer();
         sb.append(this.name() + " has " + count + " lanes \n");
         int laneNumber = 0;
-        Direction direction = Direction.STRAIGHT;
-        for(RoadElement item: layout) {
-            if(item.isHeading()) {
-                direction = item.getDirection();
-            }
-            else if(item.isLane()) {
-                laneNumber++;
-                sb.append(item + " #" + laneNumber + " width = " + item.getWidth() + " ("+direction+")\n");
-            }
-            else {
-                sb.append(item + "\n");
-            }
+        for(Lane lane: lanes) {
+            laneNumber++;
+            sb.append(" #" + laneNumber + " " + lane + "\n");
         }
         return sb.toString();
     }
